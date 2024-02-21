c58ecda9969aa05232aaf6fc6e48f28348b6601b
Alex Small <alex.small777@gmail.com>
2024-02-21 09:41:38 -0500

    class code

---
+++ my-code/BusinessClockSolution/BusinessClockApi.UnitTests/StandardBusinessClockTests.cs
@@ -0,0 +1,31 @@
+﻿namespace BusinessClockApi.UnitTests;
+public class StandardBusinessClockTests
+{
+    [Fact]
+    public void ClosedOnSaturday()
+    {
+        Assert.Fail("Not Written Yet");
+    }
+
+    [Fact]
+    public void ClosedOnSunday()
+    {
+        Assert.Fail("NotWrittenYet");
+    }
+
+    [Theory]
+    [InlineData("2/20/2023 16:25:00")]
+    [InlineData("2/20/2023 09:00:00")]
+    public void WeAreOpen(string dateTime)
+    {
+        Assert.Fail("Not Written Yet");
+    }
+
+    [Theory]
+    [InlineData("2/20/2023 17:00:00")]
+    [InlineData("2/20/2023 08:59:00")]
+    public void WeAreClosed(string dateTime)
+    {
+        Assert.Fail("Not Written Yet");
+    }
+}
---
+++ my-code/BusinessClockSolution/BusinessClockApi/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+//above this line is "internal" configuration stuff
+var app = builder.Build();
+//after this line is configuration for the "middleware"
+// -how actual HTTP requests are handled and responses are sent
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.MapGet("/support-info", ([FromServices] IProvideTheBusinessClock clock) =>
+{
+    if (clock.IsOpen())
+    {
+        return new SupportInfoResponse("Graham", "555-1212");
+    }
+    else
+    {
+        return new SupportInfoResponse("TechSupportPros", "800-STUF-BROKE");
+    }
+});
+
+app.Run();
+
+public record SupportInfoResponse(string Name, string Phone);
+
+public interface IProvideTheBusinessClock
+{
+    bool IsOpen();
+}
+
+public partial class Program { }
